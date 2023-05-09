defa78a28793009a999dafb88a4f5a54a76ccc55
Chris8331540 <mendozachristopher833@gmail.com>
2023-05-09 10:22:42 +0200

    adición de prueba mstest

---
+++ PruebaUnitariaMsTest/UnitTest1.cs
@@ -0,0 +1,47 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+using Prueba;
+using System.Diagnostics.Eventing.Reader;
+using Prueba.Servicios;
+using Prueba.Controllers;
+using System.Threading.Tasks;
+using Prueba.Modelos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PruebaUnitariaMsTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private IEnumerable<int> valoresDePrueba;
+        private readonly Servicio _servicio;
+        private readonly Asteroids asteroids;
+        public UnitTest1()
+        {
+            _servicio = new Servicio();
+            valoresDePrueba = Enumerable.Range(1, 7);
+            asteroids = new Asteroids(_servicio);
+        }
+
+
+        [TestMethod]
+        //[DynamicData(nameof(valoresDePrueba))]
+        [DataRow(1)]
+        public async Task ComprobarRealizarPeticion(int dias)
+        {
+            string json = await _servicio.RealizarPeticion(dias);
+
+            Assert.IsNotNull(json);
+        }
+
+        [TestMethod]
+        //[DynamicData(nameof(valoresDePrueba))]
+        [DataRow(0)]
+        public void ComprobarControladorPeticionIncorrecta(int dias) {
+            var response = asteroids.Get(dias).Result;
+            Assert.IsInstanceOfType(response, typeof(BadRequestObjectResult));
+        }
+    }
+
+}
---
+++ PruebasUnitariasMsTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PruebasUnitariasMsTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PruebasUnitariasMsTest")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("316f4c5f-6e36-4cc3-8e35-bbd40fa39d03")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
