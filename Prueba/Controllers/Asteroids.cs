2e2008306c9984022b53206e8a20c8008aac4e0e
Chris8331540 <mendozachristopher833@gmail.com>
2023-05-09 08:45:42 +0200

    Se ha añadido el metodo Realizar Petición de la interfaz Servicios

--- Prueba/Controllers/Asteroids.cs
+++ Prueba/Controllers/Asteroids.cs
@@ -21,8 +21,7 @@ namespace Prueba.Controllers
     [ApiController]
     public class Asteroids : ControllerBase
     {
-        private static HttpClient httpClient = new HttpClient();
-        private readonly string _rutaJson = "C:\\Users\\christopher.mendoza\\Downloads\\testJson.json";
+
         private readonly IServicio _servicio;
 
         public Asteroids(IServicio servicio)
@@ -30,27 +29,17 @@ namespace Prueba.Controllers
             _servicio = servicio;
         }
         [HttpGet("{dias:int}")]
-        public async Task<IActionResult> Get(int dias) {
+        public async Task<IActionResult> Get(int dias)
+        {
 
-            if (dias <= 0 && dias > 7) {
+            if (dias <= 0 || dias > 7)
+            {
                 return BadRequest("Cantidad de días equivocado");
             }
-            //DateTime hoy  = DateTime.Now;
-            //DateTime fechaFinal = hoy.AddDays(dias);
-            //string fechaHoyString = hoy.ToString("yyyy-MM-dd");
-            //string fechaFinalString = fechaFinal.ToString("yyyy-MM-dd");
-            //string url = $"https://api.nasa.gov/neo/rest/v1/feed?start_date={fechaHoyString}&end_date={fechaFinalString}&api_key=DEMO_KEY";
 
-            //HttpResponseMessage response = await httpClient.GetAsync(url);
-
-            //string json = await response.Content.ReadAsStringAsync();
-            string json = System.IO.File.ReadAllText(_rutaJson);
-
+            string json = await _servicio.RealizarPeticion(dias);
             List<Asteroide> asteriodes = _servicio.GetAsteroides(json);
-            
-
-            string jsonAsteriodes = JsonConvert.SerializeObject(asteriodes.OrderBy(x=>x.Diametro).Take(3).ToList());
-
+            string jsonAsteriodes = JsonConvert.SerializeObject(asteriodes.OrderBy(x => x.Diametro).Take(3).ToList());
 
             return Ok(jsonAsteriodes);
 
--- Prueba/Servicios/Servicio.cs
+++ Prueba/Servicios/Servicio.cs
@@ -6,6 +6,10 @@ using System.Collections.Generic;
 using System;
 using System.Data.SqlTypes;
 using Newtonsoft.Json;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
 
 namespace Prueba.Servicios
 {
@@ -13,10 +17,12 @@ namespace Prueba.Servicios
     public interface IServicio
     {
         List<Asteroide> GetAsteroides(string json);
+        Task<string> RealizarPeticion(int dias);
     }
     public class Servicio : IServicio
     {
-
+        private static HttpClient httpClient = new HttpClient();
+        private readonly string _rutaJson = "C:\\Users\\christopher.mendoza\\Downloads\\testJson.json";
 
         public List<Asteroide> GetAsteroides(string json)
         {
@@ -25,22 +31,39 @@ namespace Prueba.Servicios
             var listaFechas = jsonObject["near_earth_objects"];
             List<Asteroide> asteroides = new List<Asteroide>();
             var valoresListaFecha = listaFechas.Values();
-
-            foreach (var elementoFecha in valoresListaFecha)
+            if (listaFechas.HasValues)
             {
-                //var elemento = elementoFecha.Values();
-                var elementosChildren = elementoFecha.Children();
-                foreach(var elemento in elementosChildren) {
-                    var elementoObject = elemento.ToObject<ApiModel>();
-                    if (elementoObject.Is_potentially_hazardous_asteroid)
+                foreach (var elementoFecha in valoresListaFecha)
+                {
+                    //var elemento = elementoFecha.Values();
+                    var elementosChildren = elementoFecha.Children();
+                    foreach (var elemento in elementosChildren)
                     {
-                        Asteroide asteroide = ParseToAsteroide(elementoObject);
-                        asteroides.Add(asteroide);
+                        var elementoObject = elemento.ToObject<ApiModel>();
+                        if (elementoObject.Is_potentially_hazardous_asteroid)
+                        {
+                            Asteroide asteroide = ParseToAsteroide(elementoObject);
+                            asteroides.Add(asteroide);
+                        }
                     }
                 }
             }
 
+
             return asteroides;
+        }
+
+        public async Task<string> RealizarPeticion(int dias) {
+            DateTime hoy = DateTime.Now;
+            DateTime fechaFinal = hoy.AddDays(dias);
+            string start_date = hoy.ToString("yyyy-MM-dd");
+            string end_date = fechaFinal.ToString("yyyy-MM-dd");
+            string url = $"https://api.nasa.gov/neo/rest/v1/feed?start_date={start_date}&end_date={end_date}&api_key=DEMO_KEY";
+
+            HttpResponseMessage response = await httpClient.GetAsync(url);
+
+            string json = await response.Content.ReadAsStringAsync();
+            return json;
         }
 
         private static double CalcularDiametro(float min, float max)
@@ -49,7 +72,8 @@ namespace Prueba.Servicios
             return diametroMedio;
         }
 
-        private static Asteroide ParseToAsteroide(ApiModel modelo) {
+        private static Asteroide ParseToAsteroide(ApiModel modelo)
+        {
             Asteroide asteroide = new Asteroide();
             asteroide.Nombre = modelo.Name;
             asteroide.Diametro = CalcularDiametro(modelo.Estimated_Diameter.Kilometers.Estimated_diameter_min,
